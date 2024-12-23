fea72684ab03760060e4a943aa70c5ec163d9db2
Matěj Minařík <majsek555@gmail.com>
2024-12-23 03:39:56 +0100

    +getUser function

--- src/main/java/com/example/noteapp/config/MyUserDetails.java
+++ src/main/java/com/example/noteapp/config/MyUserDetails.java
@@ -9,13 +9,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.noteapp.model.User;
 
-
 public class MyUserDetails implements UserDetails {
 
     private User user;
 
     public MyUserDetails(User user) {
         this.user = user;
+    }
+
+    public User getUser() {
+        return user;
     }
 
     @Override
@@ -53,5 +56,3 @@ public class MyUserDetails implements UserDetails {
         return true;
     }
 }
-
-
